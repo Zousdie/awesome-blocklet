f99c851abcb2971bc075d2c8a3cd9b21b544ad6c
Jialiang T <rememhe@outlook.com>
2020-07-07 05:27:25 +0800

    fix(server): correct static file directory

--- server/static-front.js
+++ server/static-front.js
@@ -3,8 +3,8 @@ const path = require('path');
 const express = require('express');
 
 module.exports = (app) => {
-  app.use(express.static(path.join(__dirname, '../build')));
+  app.use(express.static(path.join(__dirname, './build')));
   app.get('/', (req, res) => {
-    res.send(fs.readFileSync(path.resolve(__dirname, '../build/index.html')).toString());
+    res.send(fs.readFileSync(path.resolve(__dirname, './build/index.html')).toString());
   });
 };
